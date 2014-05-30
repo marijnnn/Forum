68dd83a6a52160e120ada4268039bff7eb5488a4
Marijn <info@marijnverwegen.nl>
2014-05-30 22:45:44 +0200

    Methodes toegevoegd voor het verwerken van MainCategories.

--- Forum/Models/MainCategory.db.cs
+++ Forum/Models/MainCategory.db.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 namespace Forum.Models
 {
 	public partial class MainCategory
 	{
+        public static MainCategory rowToMainCategory(DataRow row)
+        {
+            return new MainCategory(Convert.ToInt32(row["MAINCATEGORY_ID"]), row["MAINCATEGORY_NAME"].ToString(), Convert.ToInt32(row["MAINCATEGORY_ORDERNUMBER"]));
+        }
+
         public static List<MainCategory> GetMainCategories()
         {
-            return new List<MainCategory>();
+            List<MainCategory> maincategories = new List<MainCategory>();
+
+            foreach (DataRow row in Database.GetData("SELECT * FROM MAINCATEGORY").Rows)
+            {
+                maincategories.Add(rowToMainCategory(row));
+            }
+
+            return maincategories;
+        }
+
+        public static MainCategory GetMainCategory(int id)
+        {
+            foreach (DataRow row in Database.GetData("SELECT * FROM MAINCATEGORY WHERE MAINCATEGORY_ID = " + id).Rows)
+            {
+                return rowToMainCategory(row);
+            }
+
+            return null;
         }
 
         public static void AddMainCategory(MainCategory maincategory)
@@ -21,6 +44,11 @@ namespace Forum.Models
                 {"@ordernumber", maincategory.OrderNumber}
             });
             maincategory.Id = id;
+        }
+
+        public static void DeleteMainCategory(MainCategory maincategory)
+        {
+            Database.Execute("DELETE FROM MAINCATEGORY WHERE MAINCATEGORY_ID = " + maincategory.Id);
         }
 	}
 }
