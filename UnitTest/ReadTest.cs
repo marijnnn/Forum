b96f75f7a4ec359034bd540705cbf3aefc7c70c3
Marijn <info@marijnverwegen.nl>
2014-05-31 20:18:30 +0200

    Simpele UnitTest om Gelezen/Ongelezen-systeem  te testen.

---
+++ UnitTest/ReadTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ReadTest
+    {
+        private UserTest usertest = new UserTest();
+
+        [TestInitialize]
+        public void CreateUser()
+        {
+            usertest.AddUser();
+            Forum.Current.User = usertest.Toegevoegd;
+        }
+
+        [TestMethod]
+        public void ReadCategory()
+        {
+            MessageTest messsagetest = new MessageTest();
+            messsagetest.AddMessage();
+
+            Assert.AreEqual(messsagetest.TopicTest.Toegevoegd.Category.IsRead(), false, "Category is read");
+            messsagetest.TopicTest.Toegevoegd.Category.MarkAsRead();
+            Assert.AreEqual(messsagetest.TopicTest.Toegevoegd.Category.IsRead(), true, "Category is unread");
+        }
+    }
+}
