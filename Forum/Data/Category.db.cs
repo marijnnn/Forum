a7d81bf43ad16fb0b4132bca57fee504f7578f76
Marijn <info@marijnverwegen.nl>
2014-05-31 19:01:25 +0200

    MinValue teruggeven indien NULL

--- Forum/Data/Category.db.cs
+++ Forum/Data/Category.db.cs
@@ -106,7 +106,7 @@ namespace Forum
         {
             DataRow row = Database.GetData("SELECT MAX(CR_DATE) LAST FROM CATEGORY_READ WHERE CR_CATEGORY_ID = " + category.Id + " AND CR_USER_ID = " + Current.User.Id).Rows[0];
 
-            return row["LAST"] == DBNull.Value ? default(DateTime) : Convert.ToDateTime(row["LAST"]);
+            return row["LAST"] == DBNull.Value ? DateTime.MinValue : Convert.ToDateTime(row["LAST"]);
         }
     }
 }
--- Forum/Data/Forum.db.cs
+++ Forum/Data/Forum.db.cs
@@ -20,7 +20,7 @@ namespace Forum
         {
             DataRow row = Database.GetData("SELECT MAX(FR_DATE) LAST FROM FORUM_READ WHERE FR_USER_ID = " + Current.User.Id).Rows[0];
 
-            return row["LAST"] == DBNull.Value ? default(DateTime) : Convert.ToDateTime(row["LAST"]);
+            return row["LAST"] == DBNull.Value ? DateTime.MinValue : Convert.ToDateTime(row["LAST"]);
         }
     }
 }
