5415fc4543d1c7f2c6841c5dfae8441c02348db6
Marijn <info@marijnverwegen.nl>
2014-06-02 13:57:04 +0200

    Inloggen.

---
+++ Forum/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Account.Login(model.Username, model.Password))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "De gebruikersnaam en/of wachtwoord is incorrect.");
+                }
+            }
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Forum/Models/AccountModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum
+{
+    public class LoginModel
+    {
+        [DisplayName("Gebruikersnaam")]
+        [Required(ErrorMessage = "Een gebruikersnaam is verplicht.")]
+        [StringLength(20, ErrorMessage = "De gebruikersnaam mag maximaal 20 tekens bevatten.")]
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        [Required(ErrorMessage = "Een wachtwoord is verplicht.")]
+        [DisplayName("Wachtwoord")]
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
