3c625378add989c6b3c2f77692bf56f75b9bb016
Marijn <info@marijnverwegen.nl>
2014-06-02 18:43:28 +0200

    ViewModels aangemaakt.

--- Forum/Models/Topic.cs
+++ Forum/Models/Topic.cs
@@ -151,24 +151,4 @@ namespace Forum
             Topic.DeleteTopic(this);
         }
     }
-
-    public class TopicModel
-    {
-        [DisplayName("Titel")]
-        [Required(ErrorMessage = "Een titel is verplicht.")]
-        [StringLength(20, ErrorMessage = "De titel mag maximaal 20 tekens bevatten.")]
-        public string Title
-        {
-            get;
-            set;
-        }
-
-        [DisplayName("Bericht")]
-        [Required(ErrorMessage = "Een bericht is verplicht.")]
-        public string Text
-        {
-            get;
-            set;
-        }
-    }
 }
---
+++ Forum/ViewModel/TopicModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum
+{
+    public class TopicModel
+    {
+        [DisplayName("Titel")]
+        [Required(ErrorMessage = "Een titel is verplicht.")]
+        [StringLength(20, ErrorMessage = "De titel mag maximaal 20 tekens bevatten.")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [DisplayName("Bericht")]
+        [Required(ErrorMessage = "Een bericht is verplicht.")]
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
