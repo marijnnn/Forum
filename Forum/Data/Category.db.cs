18c99bb3622064376afaa1d19039ff4473143a70
Marijn <info@marijnverwegen.nl>
2014-06-05 09:45:17 +0200

    SQL parameters toegepast op GetUnreadTopicCount

--- Forum/Data/Category.db.cs
+++ Forum/Data/Category.db.cs
@@ -128,11 +128,10 @@ namespace Forum
 
             if (Current.IsLoggedIn && category_ids.Count > 0)
             {
-                foreach (DataRow row in Database.GetData("SELECT TOPIC_CATEGORY_ID AS CATEGORY_ID, COUNT(1) AS AANTAL FROM TOPIC JOIN MESSAGE ON MESSAGE_ID = TOPIC_LASTMESSAGE_ID LEFT JOIN TOPIC_READ ON TR_TOPIC_ID = TOPIC_ID AND TR_USER_ID = @user_id LEFT JOIN CATEGORY_READ ON CR_USER_ID = @user_id AND CR_CATEGORY_ID = TOPIC_CATEGORY_ID WHERE (CR_DATE IS NULL OR CR_DATE < MESSAGE_DATE) AND (TR_DATE IS NULL OR TR_DATE < MESSAGE_DATE) AND TO_DATE(@forum_read, 'SYYYY-MM-DD HH24:MI:SS') < MESSAGE_DATE AND TOPIC_CATEGORY_ID IN (@categories) GROUP by TOPIC_CATEGORY_ID", new Dictionary<string, object>()
+                foreach (DataRow row in Database.GetData("SELECT TOPIC_CATEGORY_ID AS CATEGORY_ID, COUNT(1) AS AANTAL FROM TOPIC JOIN MESSAGE ON MESSAGE_ID = TOPIC_LASTMESSAGE_ID LEFT JOIN TOPIC_READ ON TR_TOPIC_ID = TOPIC_ID AND TR_USER_ID = :user_id LEFT JOIN CATEGORY_READ ON CR_USER_ID = :user_id AND CR_CATEGORY_ID = TOPIC_CATEGORY_ID WHERE (CR_DATE IS NULL OR CR_DATE < MESSAGE_DATE) AND (TR_DATE IS NULL OR TR_DATE < MESSAGE_DATE) AND TO_DATE(@forum_read, 'SYYYY-MM-DD HH24:MI:SS') < MESSAGE_DATE AND TOPIC_CATEGORY_ID IN (SELECT CATEGORY_ID FROM CATEGORY) GROUP by TOPIC_CATEGORY_ID", new Dictionary<string, object>()
                 {
-                    {"@user_id", Current.Account.Id},
-                    {"@forum_read", Forum.GetLastMarkAsRead().ToString("yyyy-MM-dd HH:mm:ss")},
-                    {"@categories", category_ids.ConvertAll<string>(x => x.ToString())}
+                    {"user_id", Current.Account.Id},
+                    {"forum_read", Forum.GetLastMarkAsRead().ToString("yyyy-MM-dd HH:mm:ss")}
                 }).Rows)
                 {
                     lijst.Add(Convert.ToInt32(row["CATEGORY_ID"]), Convert.ToInt32(row["AANTAL"]));
